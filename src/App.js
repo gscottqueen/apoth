67f96751636881e6c5f129730609c4ae2f6eea97
gscottqueen <geoffreysqueen@gmail.com>
2022-03-31 16:54:19 -0400

    include reactive width and height measurement at 1/6 scale

--- src/App.js
+++ src/App.js
@@ -1,13 +1,31 @@
+import { useState } from 'react';
 import {CabinetConfigurator} from './components'
 
 function App() {
+
+  const [width, setWidth] = useState(48)
+  const [height, setHeight] = useState(36)
+
+  function scaleMeasurement(value) {
+    return value * 96 / 6
+  }
+
   return (
     <div className="App">
       <header className="App-header">
         <h1>Apoth</h1>
       <form>
-        <CabinetConfigurator />
+        <CabinetConfigurator setWidth={setWidth} setHeight={setHeight} />
       </form>
+      <div
+        className='box'
+        width={width + 'px'}
+        height={height + 'px'}
+        style={{
+          border: "1px solid black",
+          height: scaleMeasurement(height) + 'px',
+          width: scaleMeasurement(width) + 'px'
+        }}></div>
       </header>
     </div>
   );
--- src/components/cabinet-configurator/index.js
+++ src/components/cabinet-configurator/index.js
@@ -8,29 +8,33 @@
 // type of hardware
 // type of wood
 // type of finish
-
-import {FieldSet, Select} from '../index'
+import {FieldSet, Select, Legend} from '../index'
 import { DEPTH_RANGE, WIDTH_RANGE, HEIGHT_RANGE } from '../../constants'
 
-const CabinetConfigurator = () => {
+const CabinetConfigurator = ({setWidth, setHeight}) => {
   return (
-  <FieldSet>
-    <Select
-      id="depth"
-      labelFor="Depth: "
-      options={DEPTH_RANGE}
-      unit="inches"/>
-    <Select
-      id="width"
-      labelFor="Width: "
-      options={WIDTH_RANGE}
-      unit="inches"/>
-    <Select
-      id="height"
-      labelFor="Height: "
-      options={HEIGHT_RANGE}
-      unit="inches"/>
-  </FieldSet>
+  <>
+    <Legend>Select dimensions for your cabinet.</Legend>
+    <FieldSet>
+      <Select
+        id="depth"
+        labelFor="Depth: "
+        options={DEPTH_RANGE}
+        unit="inches"/>
+      <Select
+        id="width"
+        labelFor="Width: "
+        options={WIDTH_RANGE}
+        unit="inches"
+        onChange={e => setWidth(e.target.value)}/>
+      <Select
+        id="height"
+        labelFor="Height: "
+        options={HEIGHT_RANGE}
+        unit="inches"
+        onChange={e => setHeight(e.target.value)} />
+    </FieldSet>
+  </>
   )
 }
 
