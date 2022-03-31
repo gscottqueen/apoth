a56183243133b2f493f819a91568450cf9b0fb27
gscottqueen <geoffreysqueen@gmail.com>
2022-03-31 16:17:08 -0400

    include CabinetConfigurator component in render

--- src/App.js
+++ src/App.js
@@ -1,8 +1,13 @@
+import {CabinetConfigurator} from './components'
+
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <h1>Apoth</h1>
+      <form>
+        <CabinetConfigurator />
+      </form>
       </header>
     </div>
   );
