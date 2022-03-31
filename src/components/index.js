e76edbdfe4e18f289d23c704afcef51c27c9e3af
gscottqueen <geoffreysqueen@gmail.com>
2022-03-31 16:16:47 -0400

    export default components from index

---
+++ src/components/index.js
@@ -0,0 +1,6 @@
+export { default as CabinetConfigurator } from './cabinet-configurator'
+export { default as FieldSet } from './fieldset'
+export { default as Input } from './input'
+export { default as Label } from './label'
+export { default as Legend } from './legend'
+export { default as Select } from './select'
