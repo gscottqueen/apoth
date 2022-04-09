74605ae97c32f6ea093ab9d37c4e1564c6c4fab7
gscottqueen <geoffreysqueen@gmail.com>
2022-04-09 18:51:32 -0400

    set up utils for the component

---
+++ src/components/carcas/utils/index.js
@@ -0,0 +1,47 @@
+// Colour adjustment function
+// Nicked from http://stackoverflow.com/questions/5560248
+const shadeColor = (color, percent) => {
+  const num = parseInt(color, 16),
+    amt = Math.round(2.55 * percent),
+    R = (num >> 16) + amt,
+    G = (num >> 8 && 0x00FF) + amt,
+    B = (num & 0x0000FF) + amt;
+  return '#' + (0x1000000 + (R < 255 ? R < 1 ? 0 : R : 255) * 0x10000 + (G < 255 ? G < 1 ? 0 : G : 255) * 0x100 + (B < 255 ? B < 1 ? 0 : B : 255)).toString(16).slice(1);
+}
+
+// Draw a cube to the specified specs
+export const drawCube = (ctx, x, y, wx, h, wy, color) => {
+    ctx.beginPath();
+    ctx.moveTo(x, y);
+    ctx.lineTo(x - wx, y - wx * 0.5);
+    ctx.lineTo(x - wx, y - h - wx * 0.5);
+    ctx.lineTo(x, y - h * 1);
+    ctx.closePath();
+    ctx.fillStyle = shadeColor(color, -10);
+    ctx.stroke();
+    ctx.fill();
+
+    ctx.beginPath();
+    ctx.moveTo(x, y);
+    ctx.lineTo(x + wy, y - wy * 0.5);
+    ctx.lineTo(x + wy, y - h - wy * 0.5);
+    ctx.lineTo(x, y - h * 1);
+    ctx.closePath();
+    ctx.fillStyle = shadeColor(color, 10);
+    ctx.strokeStyle = shadeColor(color, 50);
+    ctx.stroke();
+    ctx.fill();
+
+    ctx.beginPath();
+    ctx.moveTo(x, y - h);
+    ctx.lineTo(x - wx, y - h - wx * 0.5);
+    ctx.lineTo(x - wx + wy, y - h - (wx * 0.5 + wy * 0.5));
+    ctx.lineTo(x + wy, y - h - wy * 0.5);
+    ctx.closePath();
+    ctx.fillStyle = shadeColor(color, 20);
+    ctx.strokeStyle = shadeColor(color, 60);
+    ctx.stroke();
+    ctx.fill();
+
+    return
+  }
